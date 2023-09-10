24d85fa3bc28eb46bcfd5c3e89de175b5d5db4ea
Aamir Rasheed <7892721+aamirrasheed@users.noreply.github.com>
2023-09-10 13:07:46 -0700

    Added some function definition - nothing is working right now yet though. Switching back to main branch to check what the problem is with the auth emulator

--- functions-js/index.js
+++ functions-js/index.js
@@ -10,7 +10,12 @@ admin.initializeApp();
 
 const axios = require('axios');
 
-const {encodeURLforRTDB} = require('./utils');
+import { Resend } from 'resend';
+
+const {
+    encodeURLforRTDB,
+    weightedRandom
+} = require('./utils');
 
 exports.createUserInDatabaseOnSignup = functions.auth.user().onCreate((user, context) => {
     // create an entry for the user in realtime database
@@ -130,29 +135,98 @@ exports.deleteSubscription = functions.https.onCall(async (data, context) => {
     })
 })
 
-// import { Resend } from 'resend';
+/*
+This function runs on a schedule once a day at 8am PST. 
+Input: 
 
-// exports.sendEmail = functions.runWith({secrets: ["RESEND_EMAIL_API_KEY"]}).https.onRequest((req, res) => {
+Output:
+
+*/
+// exports.sendEmailAsSummarizedLinks = functions.runWith({secrets: ["RESEND_EMAIL_API_KEY"]}).https.onRequest((req, res) => {
 
 //     const resend = new Resend(process.env.RESEND_EMAIL_API_KEY);
+//     const NUM_POSTS_PER_EMAIL = 3
 
 //     // Loop over all users
-//     admin.database().ref('users').once('value')
-//     .then(snapshot => {
+//     admin.database().ref('users').once('value').then(snapshot => {
 //         const promises = [];
 //         snapshot.forEach(userSnapshot => {
 //             const userUid = userSnapshot.key;
 //             promises.push(
-//                 // get the websites the user has subscribed to, pick a random one
-//                 admin.database().ref(`users/${userUid}/subscriptions`).once('value')
-//                 .then(subscriptionSnapshot => {
+//                 // Step 1: Get three posts the user hasn't read
+//                 // Get the websites the user has subscribed to, pick a random one
+//                 admin.database().ref(`users/${userUid}/subscriptions`).once('value').then(subscriptionSnapshot => {
 //                     const subscriptions = subscriptionSnapshot.val();
-//                     const subscriptionUrls = Object.keys(subscriptions);
-//                     const randomSubscriptionUrl = subscriptionUrls[Math.floor(Math.random() * subscriptionUrls.length)];
-//                     return admin.database().ref(`websites/${randomSubscriptionUrl}/posts`).once('value');
+//                     const hashedSubscriptionUrls = Object.keys(subscriptions);
+
+
+//                     // if user has no subscriptions, throw an error to break this promise chain
+//                     if (hashedSubscriptionUrls.length === 0) {
+//                         throw new Error('User has no subscriptions');
+//                     }
+
+//                     // Randomize the order of the subscriptions
+//                     for(let i = hashedSubscriptionUrls.length - 1; i > 0; i--){
+//                         const j = Math.floor(Math.random() * i)
+//                         const temp = hashedSubscriptionUrls[i];
+//                         hashedSubscriptionUrls[i] = hashedSubscriptionUrls[j];
+//                         hashedSubscriptionUrls[j] = temp;
+//                     }
+                    
+//                     // Get posts from the users subscriptions
+//                     let promises = [];
+//                     let selectedHashedPostUrls = [];
+//                     let postsFromSelectedSubscriptions = {};
+
+//                     // Randomly select up to NUM_POSTS_PER_EMAIL subscriptions
+//                     let selectedSubscriptions = hashedSubscriptionUrls;
+//                     if (hashedSubscriptionUrls.length > NUM_POSTS_PER_EMAIL) {
+//                         // hashedSubscriptionUrls is already randomized, just get the first NUM_POSTS_PER_EMAIL
+//                         selectedSubscriptions = hashedSubscriptionUrls.slice(0, NUM_POSTS_PER_EMAIL);
+//                     }
+
+//                     // Gather all posts from selected subscriptions
+//                     for(let i = 0; i < selectedSubscriptions.length; i++) {
+//                         let promise = admin.database().ref(`websites/${selectedSubscriptions[i]}/posts`).once('value')
+//                         .then(postsSnapshot => {
+//                             const posts = postsSnapshot.val();
+//                             let hashedPostUrls = Object.keys(posts);
+                            
+//                             // this is the weighted probability of each post. We're basically weighting it so that
+//                             // each WEBSITE is equally likely to have a post selected, but each POST is equally likely
+//                             // within the WEBSITE
+//                             let eachPostProbability = 1 / selectedSubscriptions.length / hashedPostUrls.length;
+
+//                             // fill postsFromSelectedSubscriptions with hashedPostUrls as keys and eachPostProbability as each value
+//                             hashedPostUrls.forEach(hashedPostUrl => {
+//                                 postsFromSelectedSubscriptions[hashedPostUrl] = eachPostProbability;
+//                             });
+
+//                         });
+
+//                         // Add the promise to the array
+//                         promises.push(promise);
+//                     }
+
+//                     // After all promises have resolved, select NUM_POSTS_PER_EMAIL posts from allPosts
+//                     return Promise.all(promises).then(() => {
+
+//                         for(let i = 0; i < NUM_POSTS_PER_EMAIL; i++) {
+//                             if (postsFromSelectedSubscriptions.length === 0) {
+//                                 break;
+//                             }
+//                             const randomIndex = Math.floor(Math.random() * allPosts.length);
+//                             const selectedPost = allPosts[randomIndex];
+//                             // Remove the selected post from allPosts
+//                             allPosts = allPosts.filter(url => url !== selectedPost);
+//                             // Add the selected post to the selectedHashedPostUrls array
+//                             selectedHashedPostUrls.push(selectedPost);
+//                         }
+//                     });
+
 //                 })
 //                 // get a random post from that website
-//                 .then(postsSnapshot => {
+//                 .then(postsSnapshots => {
 //                     const posts = postsSnapshot.val();
 //                     const postUrls = Object.keys(posts);
 //                     const randomPostUrl = postUrls[Math.floor(Math.random() * postUrls.length)];
@@ -187,6 +261,10 @@ exports.deleteSubscription = functions.https.onCall(async (data, context) => {
 //                         post: resendData.id,
 //                         date: new Date().toISOString()
 //                     })
+//                 })
+//                 .catch(error => {
+//                     // most likely just a user with no subscriptions
+//                     console.error(error);
 //                 })
 //             );
 //         });
--- functions-js/utils.js
+++ functions-js/utils.js
@@ -4,28 +4,17 @@ function encodeURLforRTDB(url) {
     return crypto.createHash('sha256').update(url).digest('hex');
 
 }
-// function encodeUrl(url) {
-//     url = url.replace(/:/g, '%AA');
-//     url = url.replace(/\./g, '%AB');
-//     url = url.replace(/\$/g, '%AC');
-//     url = url.replace(/\[/g, '%AD');
-//     url = url.replace(/\]/g, '%AE');
-//     url = url.replace(/#/g, '%AF');
-//     url = url.replace(/\//g, '%AG');
-//     return url;
-// }
 
-// function decodeUrl(url) {
-//     url = url.replace(/%AG/g, "/");
-//     url = url.replace(/%AF/g, "#");
-//     url = url.replace(/%AE/g, "]");
-//     url = url.replace(/%AD/g, "[");
-//     url = url.replace(/%AC/g, "$");
-//     url = url.replace(/%AB/g, ".");
-//     url = url.replace(/%AA/g, ":");
-//     return url;
-// }
+function weightedRandom(prob) {
+    let i, sum = 0, r = Math.random();
+    for (i in prob) {
+        sum += prob[i];
+        if (r <= sum) return i;
+    }
+}
+
 
 module.exports = {
-    encodeURLforRTDB
+    encodeURLforRTDB,
+    weightedRandom
 }
