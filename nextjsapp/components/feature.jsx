2a0587b8713347ac94c8eda865d0ac1e8bfc826d
Aamir Rasheed <7892721+aamirrasheed@users.noreply.github.com>
2023-09-20 03:41:26 -0700

    Fixed a few bugs when landing page was displayed on mobile

--- nextjsapp/components/feature.jsx
+++ nextjsapp/components/feature.jsx
@@ -14,8 +14,8 @@ function Feature() {
                   <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
                 </svg>
               </div>
-              <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                <h2 className="text-xl title-font font-medium mb-2">Cut out the never-ending now.</h2>
+              <div className="flex-grow text-left mt-6 sm:mt-0">
+                <h2 className="text-xl text-center sm:text-left title-font font-medium mb-2">Cut out the never-ending now.</h2>
                 <p className="leading-relaxed text-base">Most people consume content produced in the last 24 hours.</p>
                 <p className="leading-relaxed text-base"> Forget about the latest viral tweet.</p>
                 <p className="leading-relaxed text-base">Read writing that's stood the test of time.</p>
@@ -30,8 +30,8 @@ function Feature() {
                   <circle cx="12" cy="7" r="4"></circle>
                 </svg>
               </div>
-              <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                <h2 className="text-xl title-font font-medium mb-2">Learn from the best minds on the internet.</h2>
+              <div className="flex-grow text-left mt-6 sm:mt-0">
+                <h2 className="text-xl title-font text-center sm:text-left font-medium mb-2">Learn from the best minds on the internet.</h2>
                 <ul>
                     <li>- <a href="http://paulgraham.com" className="text-purple-500 underline">Paul Graham</a>, founder of Y-Combinator</li>
                     <li>- <a href="https://tim.blog" className="text-purple-500 underline">Tim Ferriss</a>, world-class meta-learner and author of 4 Hour Workweek</li>
@@ -51,13 +51,13 @@ function Feature() {
                   <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
                 </svg>
               </div>
-              <div className="flex-grow sm:text-left text-center mt-6 sm:mt-0">
-                <h2 className="text-xl title-font font-medium mb-2">Take control of your information diet.</h2>
+              <div className="flex-grow text-left mt-6 sm:mt-0">
+                <h2 className="text-xl text-center sm:text-left mtitle-font font-medium mb-2">Take control of your information diet.</h2>
                 <p className="leading-relaxed text-base">On social media, recommendation algorithms feed you junk food. Your email is different - you control what you see. By bringing the right content to your inbox, you optimize for knowledge over engagement.</p>
               </div>
             </div>
             <Button
-                className="flex mx-auto mt-10 py-2 px-10 w-1/3 text-lg bg-purple-500 text-white hover:bg-purple-1000"
+                className="flex mx-auto mt-10 py-2 px-10 xl:w-1/4 lg:w-1/3 w-2/3 text-lg bg-purple-500 text-white hover:bg-purple-1000"
                 size="lg"
                 href="/login"
                 as={Link}
