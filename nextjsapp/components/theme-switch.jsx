350a12cf27508772c6822ec526a3b9be1c5c3dbe
Aamir Rasheed <7892721+aamirrasheed@users.noreply.github.com>
2023-09-28 14:43:27 -0700

    Removed the last tsx file from components folder

--- nextjsapp/components/theme-switch.jsx
+++ nextjsapp/components/theme-switch.jsx
@@ -6,12 +6,7 @@ import clsx from "clsx";
 
 import { SunFilledIcon, MoonFilledIcon } from "@/components/icons";
 
-export interface ThemeSwitchProps {
-	className?: string;
-	classNames?: SwitchProps["classNames"];
-}
-
-export const ThemeSwitch: FC<ThemeSwitchProps> = ({
+export const ThemeSwitch = ({
 	className,
 	classNames,
 }) => {
