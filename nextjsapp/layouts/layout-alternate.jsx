e190f402d4965a9c0738b1b7009bc4780fe9a68a
Aamir Rasheed <7892721+aamirrasheed@users.noreply.github.com>
2023-11-10 15:55:17 -0500

    Removed sunset banner

--- nextjsapp/layouts/layout-alternate.jsx
+++ nextjsapp/layouts/layout-alternate.jsx
@@ -4,9 +4,9 @@ import { Navbar } from "@/components/navbar";
 export default function LayoutAlternate ({ children }) {
     return (
         <>
-            <div className="bg-orange-800 text-white p-4 text-center">
+            {/* <div className="bg-orange-800 text-white p-4 text-center">
                 Athenareader has been sunsetted - contact <a href="https://twitter.com/aamir1rasheed" className="underline">Aamir</a> if you're interested in this product
-            </div>
+            </div> */}
             <Head />
             <Navbar />
             <main>{children}</main>
